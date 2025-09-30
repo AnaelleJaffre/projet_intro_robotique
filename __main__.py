18e6e876e2679d05adcd04b395b249bf0cdb9fcb
xdZireael <matisduval18magic@gmail.com>
2025-09-30 15:19:26 +0200

    better folders

--- __main__.py
+++ __main__.py
@@ -1,8 +1,18 @@
-import image-processing.setup_motors
-import step-motors/*
+import cv2
+from image_processing/shape_detection import *
 
 if __name__ == '__main__':
-    #Main Loop
-    setup_motors()
+    # Main Loop
+    cap = cv2.VideoCapture(0)
     
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
         
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
--- step_motors/motor-test.py
+++ step_motors/motor-test.py
@@ -6,14 +6,14 @@ if not ports:
     exit('No port')
 
 dxl_io = pypot.dynamixel.DxlIO(ports[0])
-dxl_io.set_wheel_mode([1])
-
+dxl_io.set_wheel_mode([1,2])
+dxl_io.enable_torque([1,2])
 print(dxl_io.get_present_position({1,2}))
 
-dxl_io.set_moving_speed({1: 360}) 
-dxl_io.set_moving_speed({2: 360})
+dxl_io.set_moving_speed({1: 400}) 
+dxl_io.set_moving_speed({2: -400})
 
-time.sleep(1)
+time.sleep(2)
 
 dxl_io.set_moving_speed({1: 0})
 dxl_io.set_moving_speed({2: 0})
