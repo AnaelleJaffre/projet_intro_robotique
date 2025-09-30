b8d4f839bca603cb84fed46b9d87dd8aae8247ac
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-09-30 11:00:02 +0200

    feat: zone segmentation + color segmentation example

---
+++ image-processing/shape_detection.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import numpy as np
+
+NUM_ZONES = 5
+"""Number of zones used during zone segmentation"""
+
+def zone_segment_by_height(img: cv.Mat):
+    """Gives a list of tuple of indexes used to segment in NUM_ZONES the image"""
+    img_height = img.shape[0]
+    sep = int(img_height / NUM_ZONES)
+    heights = [i for i in range(0, img_height, sep-1)]
+    return [(heights[i] , heights[i+1]) for i in range(len(heights)-1)]
+
+
+def center_of_zone(img, h_start, h_end):
+    """compute center of zone using start and end zones"""
+    # for the end, we reverse the array, take the maximum, and compute its location
+    z_start = (np.argmin(img[h_start]), len(img) - 1 - np.argmin(img[h_start][::-1]))
+    print(z_start)
+    z_start_center = (z_start[1] - z_start[0]) / 2
+    z_end = (np.argmin(img[h_end]), len(img) - 1 - np.argmin(img[h_end][::-1]))
+    z_end_center = (z_end[1] - z_start[0]) / 2
+
+    return (
+        (z_start_center + z_end_center) / 2,
+        (h_end - h_start) / 2
+    )
+
+
+if __name__ == '__main__':
+    # cap = cv.VideoCapture(2)
+    win_name = "main"
+    cv.namedWindow(win_name)
+    # _, frame = cap.read()
+    frame = cv.imread("sample_dark_red_line.png") # height, width, dims (or colors)
+
+    frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    lower = (0, 0, 0)
+    higher = (166, 255, 255)
+    frame_threshold = cv.inRange(frame_HSV, lower, higher)
+
+    cv.imshow(win_name, frame_threshold)
+    key = cv.waitKey()
