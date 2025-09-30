fix: last maximum computation

image-processing/shape_detection.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-NUM_ZONES = 5
+NUM_ZONES = 15
 """Number of zones used during zone segmentation"""
 
 def zone_segment_by_height(img: cv.Mat):
@@ -15,16 +15,15 @@ def zone_segment_by_height(img: cv.Mat):
 def center_of_zone(img, h_start, h_end):
     """compute center of zone using start and end zones"""
     # for the end, we reverse the array, take the maximum, and compute its location
-    z_start = (np.argmin(img[h_start]), len(img) - 1 - np.argmin(img[h_start][::-1]))
-    print(z_start)
-    z_start_center = (z_start[1] - z_start[0]) / 2
-    z_end = (np.argmin(img[h_end]), len(img) - 1 - np.argmin(img[h_end][::-1]))
-    z_end_center = (z_end[1] - z_start[0]) / 2
+    z_start = (np.argmin(img[h_start]), img.shape[1] - 1 - np.argmin(img[h_start][::-1]))
+    z_start_center = np.min(z_start) + np.abs(z_start[0] - z_start[1]) / 2
+    z_end = (np.argmin(img[h_end]), img.shape[1] - 1 - np.argmin(img[h_end][::-1]))
+    z_end_center = np.min(z_end) + np.abs(z_end[0] - z_end[1]) / 2
 
-    return (
+    return np.array((
         (z_start_center + z_end_center) / 2,
-        (h_end - h_start) / 2
-    )
+        (h_end + h_start) / 2.
+    ))
 
 
 if __name__ == '__main__':