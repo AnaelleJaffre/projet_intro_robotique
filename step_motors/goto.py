fe434866161f86a31303fd108fa6c18c16490594
guesswh0sback <quentinfallito@gmail.com>
2025-10-01 11:39:50 +0200

    parametrisation des adresses moteurs

--- step_motors/goto.py
+++ step_motors/goto.py
@@ -6,6 +6,13 @@ import pypot.dynamixel
 Rwheels = 25.85*0.001; #wheel radius in m
 Drob = 117.2*0.001; #space between both wheels in m
 
+# control parameters 
+adressMotorLeft = 1
+adressMotorRight = 2
+
+omegaMotorMove = 360 #angular speed in °/s (ie base moves )
+omegaMotorTurn = 202 #angular speed in °/s (ie base turns 90°/s )
+
 ## position rob
 XYTHETHA = [0,0,0] # movements along the Y axis 
 
@@ -29,22 +36,22 @@ def turn(Angle):
     #rotation of the wheels
     consigne_motor = Drob/Rwheels * Angle
     #set constant angular speed in opposition to turn the base
-    dxl_io.set_moving_speed({1: omegaMotor}) 
-    dxl_io.set_moving_speed({2: -omegaMotor})
+    dxl_io.set_moving_speed({adressMotorLeft: omegaMotorTurn}) 
+    dxl_io.set_moving_speed({adressMotorRight: -omegaMotorTurn})
     #time = angle/angular_speed
-    time.sleep(consigne_motor/omegaMotor)
+    time.sleep(consigne_motor/omegaMotorTurn)
     #stops the motors
-    dxl_io.set_moving_speed({1: 0}) 
-    dxl_io.set_moving_speed({2: 0})
+    dxl_io.set_moving_speed({adressMotorLeft: 0}) 
+    dxl_io.set_moving_speed({adressMotorRight: 0})
 
 def move(Lenght):
     #rotation of the wheels
     consigne_motor = 2/Rwheels * Lenght
     #set constant angular speed to move the base
-    dxl_io.set_moving_speed({1: omegaMotor}) 
-    dxl_io.set_moving_speed({2: omegaMotor})
+    dxl_io.set_moving_speed({adressMotorLeft: omegaMotorMove}) 
+    dxl_io.set_moving_speed({adressMotorRight: omegaMotorMove})
     #time = lenght/linear_speed
-    time.sleep(consigne_motor/omegaMotor)
+    time.sleep(consigne_motor/omegaMotorMove)
     #stops the motors
-    dxl_io.set_moving_speed({1: 0}) 
-    dxl_io.set_moving_speed({2: 0})
+    dxl_io.set_moving_speed({adressMotorLeft: 0}) 
+    dxl_io.set_moving_speed({adressMotorLeft: 0})
