fc9e62da079b2257ddce338517b72df3df1d5248
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-10-01 08:25:40 +0200

    feat: optimal camera parameters

---
+++ image_processing/opencv_inrange_camera_params.py
@@ -0,0 +1,19 @@
+# Min for values : 0
+# Hue max : 360 // 2
+# Other max values : 255
+
+RED = (
+    (24, 0, 70), # Lows
+    (171, 191, 255) # Highs
+)
+
+YELLOW = (
+    (90, 0, 90),
+    (180, 190, 255)
+)
+
+# kind of a light blue
+BLUE = (
+    (0, 0, 73),
+    (180, 161, 237)
+)
