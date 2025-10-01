bd382cffbd42822166335316f012f5fb6ed2418b
Matis Duval <matis.duval@u-bordeaux.fr>
2025-10-01 15:04:29 +0200

    fixed colors

--- image_processing/opencv_inrange_camera_params.py
+++ image_processing/opencv_inrange_camera_params.py
@@ -4,36 +4,36 @@ import numpy as np
 # Other max values : 255
 
 RED1 = (
-    np.array(24, 0, 70), # Lows
-    np.array(171, 191, 255) # Highs
+    np.array([24, 0, 70]), # Lows
+    np.array([171, 191, 255]) # Highs
 )
 
 RED2 = (
-    np.array(160, 100, 100), # Lows
-    np.array(180, 255, 255) # Highs
+    np.array([160, 100, 100]), # Lows
+    np.array([180, 255, 255]) # Highs
 )
 
 YELLOW1 = (
-    np.array(90, 0, 90), # Lows
-    np.array(180, 190, 255) # Highs
+    np.array([90, 0, 90]), # Lows
+    np.array([180, 190, 255]) # Highs
 )
 
 YELLOW2 = (
-    np.array(90, 0, 90), # Lows
-    np.array(180, 190, 255) # Highs
+    np.array([90, 0, 90]), # Lows
+    np.array([180, 190, 255]) # Highs
 )
 
 BLUE1 = (
-    np.array(0, 0, 73), # Lows
-    np.array(180, 161, 237) # Highs
+    np.array([0, 0, 73]), # Lows
+    np.array([180, 161, 237]) # Highs
 )
 
 BLUE2 = (
-    np.array(0, 0, 73), # Lows
-    np.array(180, 161, 237) # Highs
+    np.array([0, 0, 73]), # Lows
+    np.array([180, 161, 237]) # Highs
 )
 
 BROWN1 = ( 
-    np.array(10, 10, 20), # Lows
-    np.array(20, 255, 200) # Highs
+    np.array([10, 10, 20]), # Lows
+    np.array([20, 255, 200]) # Highs
 )
