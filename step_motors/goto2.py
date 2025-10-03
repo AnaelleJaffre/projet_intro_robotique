3214f75a870157bb5cf5cb7ff33acdceede5642e
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-10-03 14:45:00 +0200

    fix: revert to goto2 valid version blue/yellow

--- step_motors/goto2.py
+++ step_motors/goto2.py
@@ -11,9 +11,10 @@ def debug_print(*args):
     if DEBUG:
         print(*args, flush=True)
 
-# geometric parameters
-Rwheels = 25.85 ; # wheel radius in m
-Drob = 117.2 ; # space between both wheels in m
+
+## geometric parameters
+Rwheels = 25.85;  # wheel radius in m
+Drob = 117.2;  # space between both wheels in m
 
 # control parameters
 adressMotorLeft = 1
@@ -21,11 +22,11 @@ adressMotorRight = 2
 
 omegaMotorMove = 220  # angular speed in °/s (ie base moves 10 cm/s)
 omegaMotorTurn = 202  # angular speed in °/s (ie base turns 90 °/s )
-satisfactory_area = 0.01  # circle around target in m
+satisfactory_area = 1  # circle around target in m
 
 ## corr parameters
-K_lin = 0.5  # Hz
-K_ang = 0.5  # Hz
+K_lin = 1  # Hz
+K_ang = 1 # Hz
 
 lin_speed = 0
 ang_speed = 0
@@ -41,7 +42,7 @@ def inv_kin(V, O):  # V in m/s and O in °/s
 
 ##
 def turn_line(dxl_io, dY, K_cor, V0):
-    V_MAX = 0.5
+    V_MAX = 50
     Omega = K_cor * dY  # the more the gap the more the angular correction
     # try:
     #     Vlin = min(V_MAX, V0/dY) #the more the gap the less the linear correction, min to cap the lin speed in straight lines
@@ -62,7 +63,7 @@ def update_error_vector(current_pos: tuple[float, float, float],
                         target_pos: tuple[float, float, float]):  # error vector
     eps_L = np.sqrt((target_pos[0] - current_pos[0]) ** 2 + (target_pos[1] - current_pos[1]) ** 2)
     eps_T = np.rad2deg(-np.arctan2(target_pos[0] - current_pos[0], target_pos[1] - current_pos[1])) - current_pos[2]
-    debug_print(f"Expected rotation : {eps_T} deg")
+    # debug_print(f"Expected rotation : {eps_T} deg")
     return ([eps_L, eps_T])
 
 
@@ -81,13 +82,13 @@ def set_command(error_vector_pos: tuple[float, float], final_angle, dxl_io):
         debug_print("not here yet")
         # TODO: should not have to reverse angular speed (minus sign should not be needed, review IK frame)
         speedLeft, speedRight = inv_kin(lin_speed, -ang_speed)
-        debug_print("speed command :", [speedLeft, speedRight])
+        # debug_print("speed command :", [speedLeft, speedRight])
         set_speed(dxl_io, [speedLeft, speedRight])
     else:
         debug_print("arrived")
-        last_ang_speed = K_ang * (final_angle - error_vector_pos[1])  # % 360
-        speedLeft, speedRight = inv_kin(0, last_ang_speed)
-        set_speed(dxl_io, [speedLeft, speedRight])
+        # last_ang_speed = K_ang * (final_angle - error_vector_pos[1])  # % 360
+        # speedLeft, speedRight = inv_kin(0, last_ang_speed)
+        # set_speed(dxl_io, [speedLeft, speedRight])
 
 
 def asserv(target_pos, f_ech, dxl_io):
